a51f496bb30b27a2a58996bd2950534aecaddf10
Utkarsh Bhandari <bhandariutkarsh01@gmail.com>
2025-04-20 15:09:52 +0530

    removed viva.cpp

--- viva.cpp
+++ viva.cpp
@@ -1,13 +1,1 @@
-#include <iostream>
-using namespace std;
-int sumUpton(int n){
-    if(n == 0) return 0;
-    else return n + sumUpton(n - 1);
-}
-int main(){
-    int n;
-    cout << "Enter natural number : " ;
-    cin >> n;
-    cout << "Sum of natural numbers upto " << n << " is " << sumUpton(n);
-    return 0;
-}
+
