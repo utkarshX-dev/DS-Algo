7852d43e8f961ba83bd530df0d8513e4375f480e
utkarshBhandari <utkarshX-dev@users.noreply.github.com>
2025-07-19 13:59:49 +0530

    queue using linkedlist

--- lC_sol_and_templates/solutions/stack&queue/queueUsingLinkedlist.cpp
+++ lC_sol_and_templates/solutions/stack&queue/queueUsingLinkedlist.cpp
@@ -1,30 +1,24 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-/**
- * Definition of linked list
- * class Node {
- * 
- * public:
- *     int data;
- *     Node* next;
- *     Node() : data(0), next(nullptr) {}
- *     Node(int x) : data(x), next(nullptr) {}
- *     Node(int x, Node* next) : data(x), next(next) {}
- * };
- * 
- * Definition of Queue
- * struct Queue {
- *   Node* front;
- *   Node* rear;
- *   void push(int);
- *   int pop();
- *   
- *   Queue() {
- *       front = rear = NULL;
- *   }
- * };
- */
+class Node {
+public:
+    int data;
+    Node* next;
+    Node() : data(0), next(nullptr) {}
+    Node(int x) : data(x), next(nullptr) {}
+    Node(int x, Node* next) : data(x), next(next) {}
+};
+
+struct Queue {
+    Node* front;
+    Node* rear;
+    Queue() {
+        front = rear = nullptr;
+    }
+    void push(int x);
+    int pop();
+};
 
 void Queue::push(int x) {
     Node* newNode = new Node(x);
