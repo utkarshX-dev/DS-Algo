de0d71f3357b1629dd18237c0bd1c15747d72783
utkarshBhandari <utkarshX-dev@users.noreply.github.com>
2025-07-30 22:07:11 +0530

    optimised delete in bst

--- lC_sol_and_templates/solutions/binarySearchTrees/deleteNodeInBST.cpp
+++ lC_sol_and_templates/solutions/binarySearchTrees/deleteNodeInBST.cpp
@@ -9,7 +9,55 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+
+//tc: O(h), sc O(h)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
 class Solution {
+    TreeNode* f(TreeNode* root, int key) {
+        if (root == NULL) {
+            return NULL;
+        }
+        if (root->val > key) {
+            root->left = f(root->left, key);
+        } else if (root->val < key) {
+            root->right = f(root->right, key);
+        } else {
+            if (!root->right && !root->left) {
+                return NULL;
+            } else if (root->right && !root->left) {
+                return root->right;
+            } else if (root->left && !root->right) {
+                return root->left;
+            } else {
+                TreeNode* inSucc = root->right;
+                while (inSucc->left != NULL) {
+                    inSucc = inSucc->left;
+                }
+                root->val = inSucc->val;
+                root->right = deleteNode(root->right, root->val);
+            }
+        }
+        return root;
+    }
+
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) { 
+        return f(root, key); 
+    }
+};
+//tc: O(n), sc O(n)
+class Solution2 {
 private:
     vector<int>vec;
     void dfs(TreeNode* root, int key){
