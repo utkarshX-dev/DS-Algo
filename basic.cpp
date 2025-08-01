10b0c7fbfbec362318b01af432f43b184d4e4c66
utkarshBhandari <utkarshX-dev@users.noreply.github.com>
2025-08-01 14:28:33 +0530

    /

--- basic.cpp
+++ basic.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 int main(){
-    
+    priority_queue<int>pq;
+    pq.push(45);
+    pq.push(46);
+    pq.push(12);
+    pq.push(10);
+    cout << pq.top() << endl; //46
+    pq.pop();
+    cout << pq.top() << endl; //45
+    pq.pop();
+    cout << pq.top() << endl; //45
+    pq.pop();
+    cout << pq.top() << endl; //45
+    pq.pop();
+    pq.pop();
+    pq.pop();
+    pq.pop();
+    cout << "Size of priority queue : "<<  pq.size() << endl;
+    cout << "size of priority quueue : " << pq.top() << endl;
+    cout << pq.top() << endl; //45
     return 0;
 }
